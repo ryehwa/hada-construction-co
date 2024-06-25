da18e3b769eb7c0dd403af7cb128d403bb5e633e
ryehwa <rhk415@gmail.com>
2024-06-25 12:14:35 +0900

    Modify settings

--- src/main/java/com/hada/hadaConstruction/HadaConstructionApplication.java
+++ src/main/java/com/hada/hadaConstruction/HadaConstructionApplication.java
@@ -1,4 +1,4 @@
-package hada.hada_.construction;
+package com.hada.hadaConstruction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/hada/hadaConstruction/HadaConstructionApplicationTests.java
+++ src/test/java/com/hada/hadaConstruction/HadaConstructionApplicationTests.java
@@ -1,4 +1,4 @@
-package hada.hada_.construction;
+package com.hada.hadaConstruction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
