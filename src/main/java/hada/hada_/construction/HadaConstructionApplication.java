66f002d836dea482c46b1aec1d118c4a166abb62
ryehwa <rhk415@gmail.com>
2024-06-25 12:08:37 +0900

    Create project

---
+++ src/main/java/hada/hada_/construction/HadaConstructionApplication.java
@@ -0,0 +1,13 @@
+package hada.hada_.construction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadaConstructionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadaConstructionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hada/hada_/construction/HadaConstructionApplicationTests.java
@@ -0,0 +1,13 @@
+package hada.hada_.construction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadaConstructionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
