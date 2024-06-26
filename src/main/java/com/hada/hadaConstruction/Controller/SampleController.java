e97baf00a86d2e7dacc5de89bcf126837b465e33
ryehwa <rhk415@gmail.com>
2024-06-26 13:16:54 +0900

    Modify header design

--- src/main/java/com/hada/hadaConstruction/Controller/SampleController.java
+++ src/main/java/com/hada/hadaConstruction/Controller/SampleController.java
@@ -9,15 +9,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample")
 public class SampleController {
 
-    @GetMapping("/say")
+    @GetMapping("/test")
     public String test(Model model) {
         model.addAttribute("say", "Hello");
         model.addAttribute("say2", "ByeBye");
         return "test";
     }
 
-    @GetMapping("/say2")
+    @GetMapping("/test2")
     public String test2(Model model) {
+        model.addAttribute("company", "회사소개");
+        model.addAttribute("ok", "Okay");
+        return "linkTest";
+    }
+
+    @GetMapping("/test3")
+    public String test3(Model model) {
+        model.addAttribute("example", "시공사례");
+        model.addAttribute("ok", "Okay");
+        return "linkTest";
+    }
+
+    @GetMapping("/test4")
+    public String test4(Model model) {
+        model.addAttribute("ok", "Okay");
+        model.addAttribute("contact", "Contact");
+        return "linkTest";
+    }
+
+    @GetMapping("/test5")
+    public String test5(Model model) {
+        model.addAttribute("online", "온라인 견적 문의");
         model.addAttribute("ok", "Okay");
         return "linkTest";
     }
